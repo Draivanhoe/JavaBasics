f95ecc2a6d65127c90ea4bc2c2fe81dfd20fdd6f
Draivanhoe <novacheaser@yahoo.it>
2024-02-24 16:55:05 +0100

    aggiungi esercizio 1 e 2

---
+++ Esercizio01.java
@@ -0,0 +1,11 @@
+// Scrivere uno snippet di codice che, dato un triangolo identificato da base ed altezza, restituisca l'area.
+
+public class Esercizio01 {
+    public static double calcolaAreaTriangolo (double base, double altezza) {
+        return (base * altezza) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calcolaAreaTriangolo(11, 7));
+    }
+}
---
+++ Esercizio02.java
@@ -0,0 +1,9 @@
+// Dichiarare 2 caratteri e stampare come risultato la combinazione dei due caratteri, affinché siano una stringa priva di spazi.
+
+public class Esercizio02 {
+    public static void main (String[] args) {
+        char carattere1 = 'a';
+        char carattere2 = 'b';
+        System.out.println(Character.toString(carattere1) + Character.toString(carattere2));
+    }
+}
