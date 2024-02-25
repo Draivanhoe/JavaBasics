681da743dd58de2fd3a67a312bcaefdce2e59d13
Draivanhoe <novacheaser@yahoo.it>
2024-02-25 17:51:06 +0100

    Casting - 1

---
+++ Esercizio03.java
@@ -0,0 +1,10 @@
+// Casting - 1
+// Scrivere uno snippet di codice che dati 2 numeri con la virgola (double) li sommi e li stampi senza virgola (int).
+
+public class Esercizio03 {
+    public static void main (String[] args) {
+        double numero1 = 5.5;
+        double numero2 = 4.49;
+        System.out.println((int) (numero1 + numero2));
+    }
+}
