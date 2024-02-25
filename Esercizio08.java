6680c115be921f101abc526ce6f999b541a03b19
Draivanhoe <novacheaser@yahoo.it>
2024-02-25 17:53:06 +0100

    Gli operatori aritmetici - 2

---
+++ Esercizio08.java
@@ -0,0 +1,8 @@
+// Gli operatori aritmetici - 2
+// Definisci un metodo per la divisione di 3 numeri interi che restituisca il risultato che dovrà essere stampato
+
+public class Esercizio08 {
+    public static void main(String[] args) {
+        System.out.println("Non ho capito la traccia");
+    }
+}
