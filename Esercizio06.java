0da3fac98832e396c7bdc65be2aef1344de458f8
Draivanhoe <novacheaser@yahoo.it>
2024-02-25 17:52:10 +0100

    Le funzioni - 2 (correzione tutor)

---
+++ Esercizio06.java
@@ -0,0 +1,12 @@
+// Le funzioni - 2 (correzione tutor)
+// Definire una funzione che accetti in ingresso una stringa e ne restituisca la lunghezza
+
+public class Esercizio06 {
+    public static void main(String[] args) {
+        System.out.println(lunghezzaStringa("papete"));
+    }
+
+    public static int lunghezzaStringa (String unaStringa) {
+        return unaStringa.length();
+    }
+}
