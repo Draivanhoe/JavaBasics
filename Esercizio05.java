5bafa82714da499a2f99fe0ffb60ca26a63c822c
Draivanhoe <novacheaser@yahoo.it>
2024-02-25 17:51:56 +0100

    Le funzioni - 1

---
+++ Esercizio05.java
@@ -0,0 +1,12 @@
+// Le funzioni - 1
+// Definire una funzione che accetti in ingresso 2 interi e ne restituisca la somma.
+
+public class Esercizio05 {
+    public static void main(String[] args) {
+        System.out.println(sommaDiInteri(12, 3));
+    }
+
+    public static int sommaDiInteri (int intero1, int intero2) {
+        return intero1 + intero2;
+    }
+}
