880e5021bc378f8f295226ca582c7b3d216988b5
Draivanhoe <novacheaser@yahoo.it>
2024-02-25 17:51:23 +0100

    Casting - 2 (correzione tutor)

---
+++ Esercizio04.java
@@ -0,0 +1,10 @@
+// Casting - 2 (correzione tutor)
+// Dati due caratteri, restituire la somma dei loro valori ASCII.
+
+public class Esercizio04 {
+    public static void main (String[] args) {
+        char carattere1 = 'a';
+        char carattere2 = 'b';
+        System.out.println(carattere1 + carattere2);
+    }
+}
